baea0e8030b1a6379d923be7670754a72120eb00
abed12345 <abedborsa1993@gmail.com>
2022-04-29 10:20:40 -0400

    update twitter stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,8 @@ class ArticlesController < ApplicationController
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token        = ENV['TWITTER_ACCESS_TOKEN2']
-      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET2']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
 
     @article = Article.new(article_params)
